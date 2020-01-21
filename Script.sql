e09db661daf7381f98ac388eb0c4d5251a3ce4ca
TerryNight <lizkakryuchkova@gmail.com>
2020-01-21 19:13:02 +0300

    Новый файл

---
+++ Script.sql
@@ -0,0 +1,12 @@
+USE gb;
+
+
+
+
+
+-- Первое дз.Домашнее задание состояло в том, чтобы просто попробовать создать папку?
+
+CREATE TABLE courses (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name VARCHAR(150) UNIQUE
+);
